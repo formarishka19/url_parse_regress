cc7f5ec69f96327d9db484dd30814a059863d4fc
Lev Khalyapin <lev.halyapin@yandex.ru>
2023-05-31 08:35:15 +0300

    v1.0

---
+++ script.py
@@ -0,0 +1,42 @@
+import logging
+import signal
+import sys
+import requests
+
+
+def run_urls(urls):
+    """Проходим по урлам."""
+    for url in urls:
+        if not url.startswith(('https://', 'http://')):
+            url = 'https://' + url
+
+        requests.get(url, timeout=3)
+
+
+def read_urls(filename):
+    """Читаем урлы из файла."""
+    urls = set()
+
+    with open(filename, 'r', encoding='UTF-8') as file:
+        for line in file:
+            urls.add(line.strip())
+
+    return urls
+
+
+def signal_handler(signal, frame) -> None:
+    """Если остановлен принудительно (Ctrl+C)."""
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    run_urls(read_urls('urls.txt'))
