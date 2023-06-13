bfe7dad6a8f547cf09a30949395f7d0d24dc3eb7
Лев Халяпин <lev.halyapin@yandex.ru>
2023-06-13 15:56:17 +0300

    Добавлена генерация файла с инфой и параметр с количеством прогонов

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
+import datetime as dt
 from distutils.util import strtobool
+import json
 import logging
 import os
 import signal
@@ -19,10 +21,13 @@ USER_AGENT = os.getenv(
         '(KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
     )
 )
+RUNS_NUMBER = int(os.getenv('RUNS_NUMBER', default='1'))
 
 
 def run_urls(urls):
     """Проходим по урлам."""
+    global urls_count
+
     for url in urls:
         if not url.startswith(('https://', 'http://')):
             url = 'https://' + url
@@ -35,6 +40,7 @@ def run_urls(urls):
                 verify=False,
                 allow_redirects=ALLOW_REDIRECTS
             )
+            urls_count += 1
         except Exception as e:
             logging.error(e)
 
@@ -50,8 +56,25 @@ def read_urls(filename):
     return urls
 
 
+def write_json() -> None:
+    '''Сохраняем стату в json-файлик.'''
+    finish = dt.datetime.now()
+
+    info['start_time'] = start_time
+    info['finish_time'] = finish.strftime("%Y.%m.%d-%H:%M:%S")
+    info['running_time'] = str(
+        dt.datetime.combine(finish, finish.time())
+        - dt.datetime.combine(start, start.time())
+    )
+    info['visited_urls_count'] = urls_count
+
+    with open(f'info-{start_time}.json', 'w', encoding='utf-8') as file:
+        json.dump(info, file, indent=4)
+
+
 def signal_handler(signal, frame) -> None:
     """Если остановлен принудительно (Ctrl+C)."""
+    write_json()
     sys.exit(0)
 
 
@@ -60,9 +83,18 @@ signal.signal(signal.SIGTERM, signal_handler)
 
 
 if __name__ == '__main__':
+    start = dt.datetime.now()
+    start_time = start.strftime("%Y.%m.%d-%H:%M:%S")
+
+    info = {}
+    urls_count = 0
+
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     )
     logging.getLogger().setLevel(logging.DEBUG)
 
-    run_urls(read_urls('urls.txt'))
+    for _ in range(RUNS_NUMBER):
+        run_urls(read_urls('urls.txt'))
+    
+    write_json()
